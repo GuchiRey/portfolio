5cd25197dd625cbc7fadd3452b211a6d024e0651
Guchi Rey <guchii.rey25@gmail.com>
2022-12-02 14:07:35 -0300

    Update portfolio

--- src/components/Experience/experienceData.js
+++ src/components/Experience/experienceData.js
@@ -8,7 +8,7 @@ let jobExperience = [
   {
     companyName: "Software on the Road",
     role: "Web Developer",
-    roleTime: "June 2019 - Present",
+    roleTime: "June 2019 - November 2022",
     companyDescription: "Software on the Road (SOTR for short) is a software factory company with many customers from all over the world and rich experience.",
     jobDescription: "I work on the front-end creating user interfaces, app mockups and consuming RESTful APIs and on the back-end maintaining and modifying web servers as needed.",
     techStack: [mongodb, express, vuejs, nodejs],
@@ -16,8 +16,8 @@ let jobExperience = [
   },
   {
     companyName: "Building Ideas",
-    role: "Web Developer",
-    roleTime: "June 2021 - Present",
+    role: "Blockchain Developer",
+    roleTime: "September 2020 - September 2021",
     companyDescription: "Different company, same people as Software on the Road. Here we are mainly dedicated to works with the blockchain.",
     jobDescription: "I work in the management of wallets, smart contracts tester and in frontend/backend of NFT collections",
     techStack: [mongodb, express, reactjs, nodejs],
--- src/components/Projects/projectsData.js
+++ src/components/Projects/projectsData.js
@@ -1,6 +1,8 @@
 import InstagramPhoto from "../../Assets/Projects/instagram-login.png";
 import TodoAppPhoto from "../../Assets/Projects/dashboard-todo.png";
 import EcommercePhoto from "../../Assets/Projects/apollo-pic.png";
+import FantasyPhoto from "../../Assets/Projects/fantasy.png";
+
 let projects = [
   {
     title: "Stash101",
@@ -10,18 +12,25 @@ let projects = [
     id: 1,
   },
   {
+    title: "Stash101 Fantasy",
+    imgPath: FantasyPhoto,
+    description: 'Stash101 Fantasy Investing works like a sports style fantasy game, you can "draft" a "lineup" of stocks and ETFs, then compete against your friends and the public in weekly games. Within the game, you are only tracking the performance of the stocks you’ve "drafted", never investing any money, so the biggest risk you’re taking on is the competition!',
+    demoLink: "https://apps.apple.com/us/app/stash101-fantasy-investing/id1587218663",
+    id: 2,
+  },
+  {
     title: "Capable Maker",
     imgPath: TodoAppPhoto,
     description: 'Capable Maker is a project that helps teach and learn projects and procedures step by step, guiding students in their "Goldilocks Zone", which means that the projects for students are not too difficult, not too easy, but just perfect, earning achievements that unlock more advanced projects.',
     demoLink: "https://capablemaker.com/",
-    id: 2,
+    id: 3,
   },
   {
     title: "Incognito",
     imgPath: EcommercePhoto,
     description: 'The Incognito (ICON) project was created by NFT collectors, artists, and professionals with the goal of bringing something unique and special to the NFT community. The collection consists of 10,000 randomly generated and unique characters living on the Ethereum blockchain as ERC-721 tokens.',
     demoLink: "https://incognitonft.com/",
-    id: 2,
+    id: 4,
   },
 ];
 
